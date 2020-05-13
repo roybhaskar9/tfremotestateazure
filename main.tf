3e73a1a3045f0cdcf9539eef6e1e47e11a422ff2
roybhaskar9 <bhaskar@study9.com>
2020-05-13 22:46:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tstate"
+    storage_account_name  = "tstate09762"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
+}
